4e24e8aeba9f7bbc016c80ded09c764b0070e21f
pjdon <paul@pjdon.dev>
2023-11-06 15:15:46 -0500

    get annotations grouped by file

--- index.js
+++ index.js
@@ -6,57 +6,21 @@
  * @param {import('probot').Probot} app
  */
 module.exports = (app) => {
-  // app.on(["check_suite.requested", "check_run.rerequested"], check);
-
-  // async function check(context) {
-  //   const startTime = new Date();
-
-  //   // Do stuff
-  //   const { head_branch: headBranch, head_sha: headSha } =
-  //     context.payload.check_suite;
-  //   // Probot API note: context.repo() => {username: 'hiimbex', repo: 'testing-things'}
-  //   return context.octokit.checks.create(
-  //     context.repo({
-  //       name: "My app!",
-  //       head_branch: headBranch,
-  //       head_sha: headSha,
-  //       status: "completed",
-  //       started_at: startTime,
-  //       conclusion: "success",
-  //       completed_at: new Date(),
-  //       output: {
-  //         title: "Probot check!",
-  //         summary: "The check has passed!",
-  //       },
-  //     })
-  //   );
-
-  app.on("check_run.completed", async context => {
+  app.on("check_run.completed", async (context) => {
     console.log("check_run.completed", { context });
 
     const checkRunId = context.payload.check_run.id;
     const repoFullName = context.payload.repository.full_name;
     const repoDetails = getRepoDetails(repoFullName);
 
-    const run = await context.octokit.checks.get({
-      owner: "pjdon", repo: "bsc-demo",
-      check_run_id: context.payload.check_run.id
-    });
-
-    const annots = await context.octokit.checks.listAnnotations({
-      owner: "pjdon", repo: "bsc-demo",
-      check_run_id: context.payload.check_run.id
-    });
-
-    const contents = await getBlobFileContents(context, annots.data[0].blob_href);
+    const annotations = await getCheckRunFileAnnotationsByBlob(
+      context.octokit,
+      { ...repoDetails, checkRunId }
+    );
   });
+};
 
-  app.on("workflow_job.completed", async context => {
-    console.log("workflow_job.completed", { context });
-  });
-}
-
-const repoFullNamePattern = /(?<owner>[\w\-]+)\/(?<repo>[\w\-]+)/gmi;
+const repoFullNamePattern = /(?<owner>[\w\-]+)\/(?<repo>[\w\-]+)/gim;
 
 function getRepoDetails(repoFullName) {
   const match = repoFullNamePattern.exec(repoFullName);
@@ -71,35 +35,47 @@ function getRepoDetails(repoFullName) {
   const properties = { owner, repo };
 
   if (owner == null || repo == null) {
-    console.error("Failed to extract properties", { properties, repoFullName, match });
+    console.error("Failed to extract properties", {
+      properties,
+      repoFullName,
+      match,
+    });
     return null;
   }
 
   return properties;
 }
 
-const annotationsFilter = ant => (
-  !ant.message.startsWith("Process completed with exit code")
-  && ant.annotation_level.toLocalLowerCase() === "failure"
-);
+const annotationsFilter = (ant) =>
+  !ant.message.startsWith("Process completed with exit code") &&
+  ant.annotation_level.toLocaleLowerCase() === "failure";
 
-async function getCheckRunFileAnnotationsByBlob(octokit, { owner, repo, checkRunId }, annotationsFilterFunction = annotationsFilter) {
+async function getCheckRunFileAnnotationsByBlob(
+  octokit,
+  { owner, repo, checkRunId },
+  annotationsFilterFunction = annotationsFilter
+) {
   let annotations;
   try {
-    annotations = await context.octokit.checks.listAnnotations({
+    annotations = await octokit.checks.listAnnotations({
       owner,
       repo,
-      check_run_id: checkRunId
+      check_run_id: checkRunId,
     });
+
+    annotations = annotations.data.filter(annotationsFilterFunction);
   } catch (error) {
-    console.error("Failed to get annotations with octokit", { error, owner, repo, checkRunId });
+    console.error("Failed to get annotations with octokit", {
+      error,
+      owner,
+      repo,
+      checkRunId,
+    });
     return null;
   }
 
-  annotations = annotations.filter(annotationsFilterFunction);
-
   const blobMap = new Map();
-  for (const ant in annotations) {
+  for (const ant of annotations) {
     const blobHref = ant.blob_href;
     const data = {
       start_line: ant.start_line,
@@ -107,7 +83,7 @@ async function getCheckRunFileAnnotationsByBlob(octokit, { owner, repo, checkRun
       end_line: ant.end_line,
       end_column: ant.end_column,
       message: ant.message,
-      row_details: ant.row_details
+      row_details: ant.row_details,
     };
 
     if (blobMap.has(blobHref)) {
@@ -119,25 +95,36 @@ async function getCheckRunFileAnnotationsByBlob(octokit, { owner, repo, checkRun
 
   // annotations per blob with blob contents
   const hydrated = await Promise.all(
-    Array.from(blobMap.entries()).map(([blobHref, annotations]) => {
-      const blobDetails = getBlobHrefDetails(blobHref);
+    Array.from(blobMap.entries()).map(
+      ([blobHref, annotations]) =>
+        new Promise((resolve) => {
+          const blobDetails = getBlobHrefDetails(blobHref);
 
-      if (blobDetails == null) {
-        return (async () => null)();
-      }
+          if (blobDetails == null) {
+            return resolve(null);
+          }
 
-      return getBlobFileContents(octokit, blobDetails).then(content => {
-        if (content == null) {
-          return null;
-        }
+          return getBlobFileContents(octokit, blobDetails).then((content) => {
+            if (content == null) {
+              return null;
+            }
 
+            return resolve({
+              path: blobDetails.path,
+              content,
+              annotations,
+            });
+          });
+        })
+    )
+  );
 
-      })
-    })
-  )
+  return hydrated.filter((x) => x != null);
 }
 
-const githubBlobPattern = /https:\/\/github\.com\/(?<owner>[\w\-]+)\/(?<repo>[\w\-]+)\/blob\/(?<ref>\w+)\/(?<path>.+)/gmi;
+const githubBlobPattern =
+  /https:\/\/github\.com\/(?<owner>[\w\-]+)\/(?<repo>[\w\-]+)\/blob\/(?<ref>\w+)\/(?<path>.+)/gim;
+
 function getBlobHrefDetails(blobHref) {
   const match = githubBlobPattern.exec(blobHref);
   if (match == null) {
@@ -161,11 +148,19 @@ function getBlobHrefDetails(blobHref) {
 }
 
 async function getBlobFileContents(octokit, { owner, repo, ref, path }) {
+  console.log("getBlobFileContents", { owner, repo, ref, path });
   let file;
   try {
-    file = await octokit.rest.repos.getContent(properties);
+    file = await octokit.rest.repos.getContent({ owner, repo, ref, path });
   } catch (error) {
-    console.error("Failed to get blob file with octokit", { error, blobHref, owner, repo, ref, path });
+    console.error("Failed to get blob file with octokit", {
+      error,
+      blobHref,
+      owner,
+      repo,
+      ref,
+      path,
+    });
     return null;
   }
 
@@ -177,12 +172,14 @@ async function getBlobFileContents(octokit, { owner, repo, ref, path }) {
   }
 
   let contents;
+
   try {
-    contents = await fetch(download_url).then(res => res.text());
+    console.log("fetchContents", { download_url });
+    contents = fetch(download_url).then((res) => res.text());
   } catch (error) {
     console.error("Failed to fetch blob contents", { error, download_url });
     return null;
   }
 
-  return contents
+  return contents;
 }
