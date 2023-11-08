4b922c024d5907ed72522be2d83ed7e5c5d9746e
pjdon <paul@pjdon.dev>
2023-11-07 20:44:48 -0500

    refactor main file

--- src/handler.js
+++ src/handler.js
@@ -1,16 +1,9 @@
-// Checks API example
-// See: https://developer.github.com/v3/checks/ to learn more
-
 const { extractRepoInfo } = require("./github/util");
 const {
   getCheckRunFileAnnotationSetsByBlob,
   makePullSuggestionsForAnnotationSets,
 } = require("./github/helpers");
 
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Probot} app
- */
 module.exports = (app) => {
   app.on("check_run.completed", async (context) => {
     console.log("check_run.completed", { context });
@@ -33,22 +26,5 @@ module.exports = (app) => {
         annotationSets,
       }
     );
-
-    // const fileContent = annotationSets[0].content;
-    // const annotation = annotationSets[0].annotations[0];
-
-    // const res = await fixAnnotation({ fileContent, annotation });
-
-    // makeHelpPrComments(context.octokit, {
-    //   ...repoInfo,
-    //   pullNumber,
-    //   annotationSet,
-    // });
   });
 };
-
-const comment = `\
-\`\`\`suggestion
-import { html, LitElement } from "lit";
-\`\`\`\
-`;
